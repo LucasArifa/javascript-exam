ad6c2671834b24d114be3767939f1009afe02655
Lucas Arifa <lucas.arifa100@gmail.com>
2020-05-12 17:18:21 -0300

    DELETED

--- index/index.js
+++
@@ -1,8 +0,0 @@
-  function verificarCheckbox() {
-    const aceiteButton = window.document.getElementById('aceitoCheckboxId');
-if (aceiteButton.checked)  {
-  document.getElementById("fazerTesteLink").style.display = "block";
-} else {
-  document.getElementById("fazerTesteLink").style.display = "none";
-}
-  }
